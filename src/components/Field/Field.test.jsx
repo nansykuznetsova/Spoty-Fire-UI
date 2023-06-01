def1aa800710f891225df293a349a22be9b17101
Anastasiia Kuznetsova <nansyivanova@gmail.com>
2023-06-01 19:00:59 +0200

    Tests for Field

--- src/components/Field/Field.test.jsx
+++ src/components/Field/Field.test.jsx
@@ -4,12 +4,12 @@ import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import { Field } from './Field';
 
-test('should update input value when typing', async () => {
+test('should update input value when typing', () => {
   // 1. ARRANGE
   render(<Field label='Search' />);
 
   // 2. ACT
-  await userEvent.type(screen.getByPlaceholderText('Search'), 'Hello,World!')
+ userEvent.type(screen.getByPlaceholderText('Search'), 'Hello,World!')
   
   // 3. ASSERT
   expect(screen.getByPlaceholderText('Search')).toHaveValue('Hello,World!')
@@ -31,4 +31,23 @@ test('should show search icon depending of the searchBar prop', () => {
   expect(screen.getByTestId('search-icon')).toBeInTheDocument();
 });
 
+test('should render Field with value', () => {
+  // 1. ARRANGE
+  render(<Field label='Search' value='test' />);
 
+  // 3. ASSERT
+  expect(screen.getByPlaceholderText('Search')).toHaveValue('test')
+});
+
+
+test('should clear the Field and remove CloseIcon', async () => {
+  // 1. ARRANGE
+  render(<Field label='Search' value='test' />);
+
+  // 2. ACT
+  await userEvent.click(screen.getByLabelText('Click to clear the field'))
+  
+  // 3. ASSERT
+  expect(screen.getByPlaceholderText('Search')).toHaveValue('');
+  expect(screen.queryByTestId('close-icon')).not.toBeInTheDocument();
+});
